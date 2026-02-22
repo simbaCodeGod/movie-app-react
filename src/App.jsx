d77cac1fa3276e50e8551b61207a744d0386087b
simbaCodeGod <isarawadano1@gmail.com>
2026-02-22 19:53:35 +0200

    Movie App Init

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <main>
+      <div className="pattern"/>
+      <div className="wrapper">
+        <header>
+          <h1>Find Movies You'll Enjoy Without the hassle</h1>
+        </header>
+      </div>
+    </main>
+  );
+};
+
+export default App;
